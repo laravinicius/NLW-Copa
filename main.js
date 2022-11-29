fcd2711b826a3f4bc391278b84682153ce17d8a8
Vinicius Lara <viniciusfelipelara@gmail.com>
2022-11-29 08:56:23 -0300

    animations

--- main.js
+++ main.js
@@ -7,9 +7,11 @@ function createGame(player1, hour, player2) {
    </li>`;
 }
 
+let delay = -0.4;
 function createCard(date, day, games) {
+   delay = delay + 0.4;
    return `
-   <div class="card">
+   <div class="card" style="animation-delay: ${delay}s">
    <h2>${date} <span>${day}</span></h2>
    <ul>
       ${games}
@@ -17,21 +19,16 @@ function createCard(date, day, games) {
    </div>`;
 }
 
-document.querySelector('#app').innerHTML = `
-   <header>
-      <img src="./assets/logo.svg" alt="">
-   </header>
-   <main id="cards">
-      ${createCard('24/11', 'quinta', 
-         createGame('switzerland', '07:00', 'cameroon') +
-         createGame('brazil', '16:00', 'serbia') 
-      )}  
-      ${createCard('28/11', 'segunda', 
-         createGame('cameroon', '07:00', 'serbia') +
-         createGame('brazil', '13:00', 'switzerland')
-      )}  
-      ${createCard('02/12', 'sexta', 
-         createGame('cameroon', '16:00', 'brazil') +
-         createGame('serbia', '16:00', 'switzerland')
-      )}  
-   </main>`;
+document.querySelector('#cards').innerHTML = `
+   ${createCard('24/11', 'quinta', 
+   createGame('switzerland', '07:00', 'cameroon') +
+   createGame('brazil', '16:00', 'serbia') 
+   )}  
+   ${createCard('28/11', 'segunda', 
+      createGame('cameroon', '07:00', 'serbia') +
+      createGame('brazil', '13:00', 'switzerland')
+   )}  
+   ${createCard('02/12', 'sexta', 
+      createGame('cameroon', '16:00', 'brazil') +
+      createGame('serbia', '16:00', 'switzerland')
+   )}`;
